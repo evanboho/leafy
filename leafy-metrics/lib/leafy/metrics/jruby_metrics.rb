0c7dceb0ee6c443734ac2ada34fc360cd4aa6784
Christian Meier <m.kristian@web.de>
2015-04-13 19:56:19 +0530

    added some yard-docs [skip ci]

--- leafy-metrics/lib/leafy/metrics/jruby_metrics.rb
+++ leafy-metrics/lib/leafy/metrics/jruby_metrics.rb
@@ -4,6 +4,10 @@ module Leafy
   module Metrics
     module JRubyMetrics
 
+      # registers a few thread gauges under the given basename: total.com, executing.count and sleeping.count
+      #
+      # @param [Leafy::Metrics::Registry] where to register the metrics
+      # @param [String] basename
       def self.register_ruby_thread_state_gauges( registry, name )
         registry.register_gauge( name + '.total.count' ) do
           Thread.list.size
