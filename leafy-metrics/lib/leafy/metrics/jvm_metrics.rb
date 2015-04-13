25affb3b2190518cb39e6a6b4dd2069a36a83e8b
Christian Meier <m.kristian@web.de>
2015-04-13 20:02:55 +0530

    added some yard-docs [skip ci]

--- leafy-metrics/lib/leafy/metrics/jvm_metrics.rb
+++ leafy-metrics/lib/leafy/metrics/jvm_metrics.rb
@@ -3,19 +3,36 @@ require 'leafy/metrics'
 module Leafy
   module Metrics
     module JvmMetrics
+
+      # registers some buffer pool metrics. more details see https://github.com/dropwizard/metrics/blob/master/metrics-jvm/src/main/java/com/codahale/metrics/jvm/BufferPoolMetricSet.java#L19
+      #
+      # @param [Leafy::Metrics::Registry] where to register the metrics
+      # @param [String] basename
       def self.register_buffer_pool_metrics( registry, name )
         # use the same MBenServer as JRuby does
         registry.register_metric( name, com.codahale.metrics.jvm.BufferPoolMetricSet.new( java.lang.management.ManagementFactory.getPlatformMBeanServer ) )
       end
 
+      # registers some memory usage gauges. more details see https://github.com/dropwizard/metrics/blob/master/metrics-jvm/src/main/java/com/codahale/metrics/jvm/MemoryUsageGaugeSet.java#L18
+      #
+      # @param [Leafy::Metrics::Registry] where to register the metrics
+      # @param [String] basename
       def self.register_memory_usage_gauges( registry, name )
         registry.register_metric( name, com.codahale.metrics.jvm.MemoryUsageGaugeSet.new )
       end
 
+      # registers some garbage collection metrics. more details see https://github.com/dropwizard/metrics/blob/master/metrics-jvm/src/main/java/com/codahale/metrics/jvm/GarbageCollectorMetricSet.java#L15
+      #
+      # @param [Leafy::Metrics::Registry] where to register the metrics
+      # @param [String] basename
       def self.register_garbage_collector_metrics( registry, name )
         registry.register_metric( name, com.codahale.metrics.jvm.GarbageCollectorMetricSet.new )
       end
 
+      # registers some (java) thread state gauges. more details see https://github.com/dropwizard/metrics/blob/master/metrics-jvm/src/main/java/com/codahale/metrics/jvm/ThreadStatesGaugeSet.java#L18
+      #
+      # @param [Leafy::Metrics::Registry] where to register the metrics
+      # @param [String] basename
       def self.register_thread_state_gauges( registry, name )
         registry.register_metric( name, com.codahale.metrics.jvm.ThreadStatesGaugeSet.new )
       end
